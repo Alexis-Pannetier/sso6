5b8505244629a949a12bcd6345dd33300bf2d0de
Alexis Pannetier <alexis.pannetier@hotmail.fr>
2022-04-20 16:10:37 +0200

    💄 New design + meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../res/styles/globals.css";
 
+import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({
@@ -8,6 +9,35 @@ export default function App({
 }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="keywords" content="sso6, single, sign-on" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:title" content="SSO6" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://sso6.herokuapp.com" />
+        <meta property="og:image:width" content="480" />
+        <meta property="og:image:height" content="400" />
+        <meta
+          property="og:image"
+          content="https://sso6.herokuapp.com/sausage.gif"
+        />
+        <meta
+          property="og:video"
+          content="https://sso6.herokuapp.com/sausage.gif"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://sso6.herokuapp.com" />
+        <meta property="twitter:title" content="SSO6" />
+        <meta
+          name="twitter:image"
+          content="https://sso6.herokuapp.com/sausage.gif"
+        />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
